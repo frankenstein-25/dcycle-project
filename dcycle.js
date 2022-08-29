03735f469e0e76a9fe1573f755021e367bd66c05
himanshu <bhadaniahh@gmail.com>
2022-08-29 14:53:15 +0530

    -a

---
+++ dcycle.js
@@ -0,0 +1,10 @@
+$("document").ready(function () {
+  $('.slick-bar').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+  });
+})
